87dd06c64135b7e801fd22fadf60ac47fb77309b
gl <g.lewarne@gmx.co.uk>
2015-05-27 16:29:43 +0100

    cpufreq: reduce frequency transition latency

--- drivers/cpufreq/exynos-mp-cpufreq.c
+++ drivers/cpufreq/exynos-mp-cpufreq.c
@@ -1150,7 +1150,7 @@ static int exynos_cpufreq_cpu_init(struct cpufreq_policy *policy)
 	cpufreq_frequency_table_get_attr(exynos_info[cur]->freq_table, policy->cpu);
 
 	/* set the transition latency value */
-	policy->cpuinfo.transition_latency = 100000;
+	policy->cpuinfo.transition_latency = 20000;
 
 	if (cpumask_test_cpu(policy->cpu, &cluster_cpus[CL_ONE])) {
 		cpumask_copy(policy->cpus, &cluster_cpus[CL_ONE]);
